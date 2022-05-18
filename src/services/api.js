f85f8b69b56952bdeafd6cb7b73706a534a014f0
Kristofer Krindges <thecontt06@gmail.com>
2022-05-18 20:20:39 -0300

    fix cards and register

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "https://q4eorgmov1.execute-api.sa-east-1.amazonaws.com/Prod",
+	baseURL: "https://m70ooitfj2.execute-api.sa-east-1.amazonaws.com/Prod",
 	withCredentials: false,
 });
 
